d561ab4201dcc0febe76c5a976ec09da5605d732
Time-Emperor <tdulal68@gmail.com>
2025-11-22 14:02:35 +0545

    Update GradeEvaluator.java

--- GradeEvaluator.java
+++ GradeEvaluator.java
@@ -3,9 +3,9 @@ public class GradeEvaluator {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
-        System.out.println("Enter the grade");
+        System.out.println("Enter the grade:");
         double grade=sc.nextDouble();
-        String store=(grade>=40) ? "pass" : "false";
+        String store=(grade>=40) ? "pass" : "fail";
         System.out.println(store);
     }
 }
